dda8a698424cad179d3c3df3a0108b9a8402a3e3
saleh mostafa 11 <120852885+salehmostafa11@users.noreply.github.com>
2025-11-25 22:03:25 +0200

    GlobalUsinsAndGeneralResponse

---
+++ GreenEye/Dto/GeneralResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace GreenEye.Dto
+{
+    public class GeneralResponse<T>
+    {
+        bool IsSuccess { get; set; }
+        string? Message { get; set; }
+        T? Data { get; set; }
+    }
+}
